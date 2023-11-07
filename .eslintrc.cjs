a90394d0a1a00de9317b818d2ff40d2f25de2188
andron13 <andron13@gmail.com>
2023-11-07 11:29:00 +0100

    fix: eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,88 +1,54 @@
 module.exports = {
   root: true,
-  env: {browser: true, es2020: true},
+  env: {
+    browser: true, es2021: true
+  },
   extends: [
-    'airbnb',
-    'airbnb-typescript',
-    'airbnb/hooks',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:import/recommended',
-    'prettier',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts', 'prettier.config.js', '@typescript-eslint', 'node_modules', 'tailwind.config.js'],
-  plugins: ['react', 'react-refresh', 'simple-import-sort', 'import', 'prettier'],
+  plugins: ['@typescript-eslint', 'import', 'react', 'react-hooks', 'react-refresh', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    project: ['tsconfig.json'],
-    tsconfigRootDir: __dirname,
-    ecmaVersion: 'latest',
     sourceType: 'module',
   },
   rules: {
+    "import/order": ["error", {
+      "groups": ["builtin", "external", "parent", "sibling", "index"],
+      "newlines-between": "always",
+      "alphabetize": {
+        "order": "asc",
+        "caseInsensitive": true
+      }
+    }],
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [1, {"extensions": [".js", ".jsx", ".ts", ".tsx"]}],
     "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-explicit-any": "off",
     "no-console": "warn",
-    'prefer-arrow-callback': 0,
-    'jsx-a11y/click-events-have-key-events': 0,
-    'jsx-a11y/no-noninteractive-element-interactions': 0,
-    'react/jsx-props-no-spreading': 0,
-    'react/require-default-props': 0,
-    'react/react-in-jsx-scope': 0,
-    'react-refresh/only-export-components': ['warn', {allowConstantExport: true}],
     'no-void': 0,
     'no-param-reassign': 0,
-    'import/no-extraneous-dependencies': 0,
-    'import/extensions': [
-      'error',
-      'ignorePackages',
+    "array-element-newline": [
+      "error",
       {
-        'ts': 'always',
-        'tsx': 'always'
+        "ArrayExpression": "consistent",
+        "ArrayPattern": {
+          "minItems": 3
+        }
       }
-    ],
-    'import/namespace': 0,
-    'sort-imports': ['error', {ignoreCase: true, ignoreDeclarationSort: true}],
-    'import/order': [
-      'error',
-      {
-        groups: [
-          ['external', 'builtin'],
-          'internal',
-          ['sibling', 'parent'],
-          'index',
-        ],
-        pathGroups: [
-          {
-            pattern: '@(react|react-native)',
-            group: 'external',
-            position: 'before',
-          },
-          {
-            pattern: '@src/**',
-            group: 'internal',
-          },
-        ],
-        pathGroupsExcludedImportTypes: ['internal', 'react'],
-        'newlines-between': 'always',
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-      },
-    ],
+    ]
+
   },
   settings: {
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true,
-      },
-      react: {
-        version: 'detect',
-      },
+    react: {
+      version: 'detect',
     },
-  },
+  }
 };
